af0ded987c8b25a11ea26336e90ca7873f843178
Theo Grace <theo.grace@hotmail.co.uk>
2022-11-07 12:54:33 +0000

    Make Interlayer function more concise

--- Kitaev_only_mean_field/Meanfieldcalc.jl
+++ Kitaev_only_mean_field/Meanfieldcalc.jl
@@ -430,20 +430,14 @@ function Hamiltonian_interlayer(Mean_fields,J_perp)
     H_perp = zeros(16,16)
     for alpha = 1:3
         for j = [0,4]
-            H_perp[1+j,1+j+alpha] = Mean_fields[9+j,9+j+alpha,1] # Adds term associated to mean spin <S_2A^alpha>
-            #M[5,5+alpha] = Mean_fields[13,13+alpha] # Adds term associated to mean spin <S_2B^alpha>
-            H_perp[9+j,9+j+alpha] = Mean_fields[1+j,1+j+alpha,1] # Adds term associated to mean spin <S_1A^alpha>
-            #M[13,13+alpha] = Mean_fields[5,5+alpha] # Adds term associated to mean spin <S_1B^alpha>
-
+            H_perp[1+j,1+j+alpha] = Mean_fields[9+j,9+j+alpha,1] # Adds term associated to mean spin <S_2^alpha> for A and B sites
+            H_perp[9+j,9+j+alpha] = Mean_fields[1+j,1+j+alpha,1] # Adds term associated to mean spin <S_1^alpha> for A and B sites 
+            
             H_perp[1+j,9+j] += -Mean_fields[1+j+alpha,9+j+alpha,1]
             H_perp[1+j,9+j+alpha] = Mean_fields[1+j+alpha,9+j,1]
             H_perp[1+j+alpha,9+j] = Mean_fields[1+j,9+j+alpha,1]
             H_perp[1+j+alpha,9+j+alpha] = -Mean_fields[1+j,9+j,1]
 
-            #M[5,13] = -Mean_fields[5+alpha,13+alpha]
-            #M[5,13+alpha] = Mean_fields[5+alpha,13]
-            #M[5+alpha,13] = Mean_fields[5,13+alpha]
-            #M[5+alpha,13+alpha] = -Mean_fields[5,13]
         end 
     end
 
