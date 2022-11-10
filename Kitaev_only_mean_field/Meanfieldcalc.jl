8cb15e513f182303a516adee324150c6e3bb679e
Theo Grace <theo.grace@hotmail.co.uk>
2022-11-10 14:07:19 +0000

    Corrects sign error in H_G function

--- Kitaev_only_mean_field/Meanfieldcalc.jl
+++ Kitaev_only_mean_field/Meanfieldcalc.jl
@@ -161,7 +161,7 @@ function Hamiltonian_G(mean_fields,k,nn,G=1)
             M_G[5,5+beta,alpha] = mean_fields[1,1+gamma,alpha] # adds terms associated with <S_i>
             M_G[1,1+beta,alpha] = mean_fields[5,5+gamma,alpha] # adds terms associated with <S_j>
             M_G[1,5,alpha] = -mean_fields[1+beta,1+gamma,alpha] # adds terms mixing gauge majoranas <iX^bX^c>
-            M_G[1+beta,5+gamma,alpha] = mean_fields[1,5,alpha] # adds terms u^0_ij 
+            M_G[1+beta,5+gamma,alpha] = -mean_fields[1,5,alpha] # adds terms u^0_ij 
             M_G[1,5+beta,alpha] = mean_fields[1+gamma,5,alpha] # adds sector mixing terms m'_ij
             M_G[1+beta,5,alpha] = mean_fields[1,1+gamma,alpha] # adds sector mixing terms m_ij
         end
