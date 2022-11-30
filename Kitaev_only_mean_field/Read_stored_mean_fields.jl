22f042837104493d1fa69879f5471c93d22afa0e
Theo Grace <theo.grace@hotmail.co.uk>
2022-11-30 13:17:29 +0000

    Rewrites read data script into single function

--- Kitaev_only_mean_field/Read_stored_mean_fields.jl
+++ Kitaev_only_mean_field/Read_stored_mean_fields.jl
@@ -1,34 +1,46 @@
 using HDF5 
 using LinearAlgebra
 
-println("Which parameter scan do you want to read?")
-println("Choices: J , G , J_perp")
-scan_type = string(readline(stdin))
-display(scan_type)
-doc_name = "parameter_scan_data/"*scan_type*"_scans"
-display(doc_name)
+function read_stored_data(scan_type)
+    """
+    Takes scan_type as an argument which can only be "J" "G" or "J_perp"
+    """
+    doc_name = "parameter_scan_data/"*scan_type*"_scans"
+    display(doc_name)
+    
+    fid = h5open(doc_name,"r")
+    show(stdout,"text/plain",keys(fid))
 
-fid = h5open(doc_name,"r")
-display(keys(fid))
+    read_another=true
 
-println("Which set of parameters do you want to read?") 
-group_name = readline()
+    while read_another == true
+        println("Which file do you want to read? Copy the file name. Type N to quit") 
+        group_name = readline()
 
-while !(group_name in keys(fid))
-    println("Not found, try again.")
-    global group_name = readline()
+        if group_name == "N"
+            break
+        end
+
+        while !(group_name in keys(fid))
+            println("Not found, try again.")
+            group_name = readline()
+        end
+
+        g = fid[group_name]
+
+        read_fields = read(g["output_mean_fields"])
+        read_description = read(g["Description_of_run"])
+        read_coupling_list = read(g[scan_type*"_list"])
+        read_marked_with_x = read(g["marked_with_x_list"])
+
+        title_str = "Varying "*scan_type*" with"*group_name
+        xlab = scan_type*" /|K|"
+
+        corrected_fields , corrected_coupling_list = remove_marked_fields(read_fields,read_coupling_list,read_marked_with_x)
+        plot_mean_fields_vs_coupling(corrected_fields,corrected_coupling_list,title_str,xlab)
+        plot_oscillating_fields_vs_coupling(read_fields,read_marked_with_x,read_coupling_list)
+    end 
+    close(fid)
+
+    return read_fields , read_description , read_coupling_list , read_marked_with_x 
 end
-g = fid[group_name]
-
-read_fields = read(g["output_mean_fields"])
-read_description = read(g["Description_of_run"])
-read_coupling_list = read(g[scan_type*"_list"])
-read_marked_with_x = read(g["marked_with_x_list"])
-close(fid)
-
-title_str = "Varying "*scan_type*" with"*group_name
-xlab = scan_type*" /|K|"
-
-corrected_fields , corrected_coupling_list = remove_marked_fields(read_fields,read_coupling_list,read_marked_with_x)
-plot_mean_fields_vs_coupling(corrected_fields,corrected_coupling_list,title_str,xlab)
-plot_oscillating_fields_vs_coupling(read_fields,read_marked_with_x,read_coupling_list)
