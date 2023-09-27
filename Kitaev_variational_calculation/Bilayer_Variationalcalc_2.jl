86e829fba39e11d7a3f487a5409067ffdbabadd1
Theo Grace <theo.grace@postgrad.manchester.ac.uk>
2023-09-27 17:38:39 +0100

    Add function to find X and Y matrices

--- Kitaev_variational_calculation/Bilayer_Variationalcalc_2.jl
+++ Kitaev_variational_calculation/Bilayer_Variationalcalc_2.jl
@@ -1,9 +1,14 @@
 # This is the version of the variational calculation for the bilayer Kitaev model as of 26/09/23
 
+# This version uses SVD of the M matrix rather than eigen decomposition of the Hamiltonian
+# This optimises the code for the gapless Kitaev model 
+# This version will also calculate the correct signs for hopping amplitudes to properly account for interference effects
+
 using LinearAlgebra
 using SparseArrays
 using Arpack 
 using PyPlot
+using SkewLinearAlgebra
 pygui(true) 
 
 # sets the real space lattice vectors
@@ -94,10 +99,10 @@ end
 
 function flip_bond_variable(M,L1,L2,m,bond_site,bond_flavour)
     """
-    Given a Hamiltonian H this returns a new Hamiltonian with a reversed sign for the bond variable at site bond_site with orientation bond_flavour  
+    Given part of the Hamiltonian M this returns a new M with a reversed sign for the bond variable at site bond_site with orientation bond_flavour  
     """
 
-    C_A_index = 1 + bond_site[1] + N*bond_site[2]
+    C_A_index = 1 + bond_site[1] + L1*bond_site[2]
 
     if bond_flavour == "z"
         C_B_index = C_A_index
@@ -123,3 +128,26 @@ end
 L1 = 20
 L2 = 20 
 m = 1
+
+function get_X_and_Y(L1,L2,m)
+    M0 = get_M0(L1,L2,m)
+
+    M1 = flip_bond_variable(M0,L1,L2,m,[1,1],"z") # M1 has z link flipped 
+    M2 = flip_bond_variable(M0,L1,L2,m,[1,1],"x") # M2 has x link flipped at the same site
+
+    F1 = svd(M1)
+    F2 = svd(M2)
+
+    U1 = F1.U
+    U2 = F2.U
+    V1 = (F1.Vt)'
+    V2 = (F2.Vt)'
+
+    U12 = U1'*U2
+    V12 = V1'*V2
+
+    X12 = 0.5(U12+V12)
+    Y12 = 0.5(U12-V12)
+
+    return X12, Y12
+end 
