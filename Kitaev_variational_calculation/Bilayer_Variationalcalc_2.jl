0471dab7a10b62890c498ac9f294ae58f0d13227
Theo Grace <theo.grace@postgrad.manchester.ac.uk>
2023-09-28 17:47:28 +0100

    Add function to calculate Heisenberg Hopping

--- Kitaev_variational_calculation/Bilayer_Variationalcalc_2.jl
+++ Kitaev_variational_calculation/Bilayer_Variationalcalc_2.jl
@@ -22,6 +22,12 @@ nx = (a2 - 2a1)/3
 
 nn = [nx,ny,nz] # stores the nearest neighbours in a vector
 
+# sets default boundary conditions
+L1 = 4
+L2 = 4
+m = 0 
+BCs = [L1,L2,m]
+
 function dual(A1,A2)
     """
     Calculates the 2D dual vectors for a given set of 2D lattice vectors 
@@ -59,7 +65,7 @@ function brillouinzone(g1, g2, N, half=true)
     return [(ix+0.5)*dx + (iy+0.5)*dy for ix in lowerx:upper, iy in lowery:upper]
 end
 
-function get_M0(L1,L2,M)
+function get_M0(BCs)
     """
     Calculates a L1L2 x L1L2 matrix M which is part of the Hamiltonian for a flux free Kitaev model in terms of Majorana fermions 
 
@@ -71,6 +77,10 @@ function get_M0(L1,L2,M)
 
     This assumes periodic boundary conditions with a torus with basis L1*a1, L2*a2 + M*a1
     """
+    L1 = BCs[1]
+    L2 = BCs[2]
+    M = BCs[3]
+
     N = L1*L2
     A = zeros(L1,L1)
     B = zeros(L1,L1)
@@ -97,10 +107,13 @@ function get_M0(L1,L2,M)
     return M 
 end 
 
-function flip_bond_variable(M,L1,L2,m,bond_site,bond_flavour)
+function flip_bond_variable(M,BCs,bond_site,bond_flavour)
     """
     Given part of the Hamiltonian M this returns a new M with a reversed sign for the bond variable at site bond_site with orientation bond_flavour  
     """
+    L1 = BCs[1]
+    L2 = BCs[2]
+    m = BCs[3]
 
     C_A_index = 1 + bond_site[1] + L1*bond_site[2]
 
@@ -119,21 +132,19 @@ function flip_bond_variable(M,L1,L2,m,bond_site,bond_flavour)
             C_B_index = C_A_index -1 
         end 
     end 
+    M_flipped = M
+    M_flipped[C_A_index,C_B_index] = -1
+    #M[C_A_index,C_B_index] = 1
 
-    M[C_A_index,C_B_index] = - M[C_A_index,C_B_index]
-
-    return M
+    # NOTE: There's a bug here. Using this function seems to change the input M as well as the output for some reason. 
+    return M_flipped
 end 
 
-L1 = 20
-L2 = 20 
-m = 1
-
-function get_X_and_Y(L1,L2,m)
-    M0 = get_M0(L1,L2,m)
-
-    M1 = flip_bond_variable(M0,L1,L2,m,[1,1],"z") # M1 has z link flipped 
-    M2 = flip_bond_variable(M0,L1,L2,m,[1,1],"x") # M2 has x link flipped at the same site
+function get_X_and_Y(BCs)
+    M0 = get_M0(BCs)
+    M1 = flip_bond_variable(M0,BCs,[1,1],"z") # M1 has z link flipped 
+    M0 = get_M0(BCs)
+    M2 = flip_bond_variable(M0,BCs,[1,2],"z") # M2 has x link flipped at the same site
 
     F1 = svd(M1)
     F2 = svd(M2)
@@ -150,4 +161,85 @@ function get_X_and_Y(L1,L2,m)
     Y12 = 0.5(U12-V12)
 
     return X12, Y12
+end 
+
+function get_Heisenberg_hopping(BCs)
+
+    initial_flux_site = [0,0]
+    M0 = get_M0(BCs)
+    F0 = svd(M0)
+    U0 = F0.U
+    V0 = F0.V 
+
+    M1 = flip_bond_variable(M0,BCs,initial_flux_site,"z") # M1 has z link flipped 
+    M0 = get_M0(BCs)
+    M2 = flip_bond_variable(M0,BCs,initial_flux_site+[1,0],"z") 
+
+    F1 = svd(M1)
+    F2 = svd(M2)
+
+    U1 = F1.U
+    U2 = F2.U
+    V1 = (F1.Vt)'
+    V2 = (F2.Vt)'
+
+    U12 = U1'*U2
+    V12 = V1'*V2
+
+    X12 = 0.5(U12+V12)
+    Y12 = 0.5(U12-V12)
+
+    M12 = inv(X12)*Y12
+
+    U21 = U2'*U1
+    V21 = V2'*V1
+
+    X21 = 0.5(U21+V21)
+    Y21 = 0.5(U21-V21)
+
+    M21 = inv(X21)*Y21
+    display(M21)
+
+    X1=0.5*(U1'+V1')
+    Y1=0.5*(U1'-V1')
+    T1= [X1 Y1 ; Y1 X1]
+    M1 = inv(X1)*Y1
+
+    X2 = 0.5*(U2'+V2')
+    Y2 = 0.5*(U2'-V2')
+    T2 = [X2 Y2 ;Y2 X2]
+    T = T2*T1'
+    M2 = inv(X2)*Y2
+
+    X = T[1:Int(size(T)[1]/2),1:Int(size(T)[1]/2)]
+    Y = T[1:Int(size(T)[1]/2),(Int(size(T)[1]/2)+1):end]
+    M = inv(X)*Y
+
+    initial_C_A_index = 1 + initial_flux_site[1] + L1*initial_flux_site[2]
+
+    hop = abs(det(X12))^(0.5)*(((U1*M21*V1')-(U1*V1'))[initial_C_A_index+1,initial_C_A_index]+1)
+    
+    display(abs(det(X12))^(0.5))
+    #display((U12*V12'-(U12*V12')'))
+    #display(pfaffian(V12*U12'-(V12*U12')'))
+
+    U10 = U1'*U0
+    V10 = V1'*V0
+    U20 = U2'*U0
+    V20 = V2'*V0
+
+    X10 = 0.5*(U10+V10)
+    Y10 = 0.5*(U10-V10)
+    X20 = 0.5*(U20+V20)
+    Y20 = 0.5*(U20-V20)
+
+    M10 = inv(X10)*Y10
+    M20 = inv(X20)*Y20
+    display(det(X10)*(det(I-M20*M10)^(0.5)))
+    display(abs(det(X10))*(pfaffian([0.5*(M20-M20') -I ; I -0.5*(M10-M10') ])))
+
+    display(pfaffian([0.5*(M20-M20') -I ; I -0.5*(M10-M10') ]))
+    display(det([0.5*(M20-M20') -I ; I -0.5*(M10-M10') ])^(0.5))
+    
+    return M21
 end 
